2b48780e940bb8d8201d6b252d2ea517dbd09bea
NikhilMahalingam <mahalingamnikhil@gmail.com>
2022-11-20 23:57:27 -0500

    Initialization

---
+++ index.py
@@ -0,0 +1,16 @@
+#install dependencies using these:
+#python3 -m pip install opencv-python
+#pip install tensorflow
+#pip install numpy
+
+
+import tensorflow as tf
+import os
+import uuid
+import time
+import cv2
+
+labels = ['Nick Chen']
+number_imgs = 10
+
+
