367300a65667ee20e69a46da05cc1b2f38dd1e10
NikhilMahalingam <mahalingamnikhil@gmail.com>
2022-11-21 00:03:39 -0500

    Updated Dependencies

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
-#install dependencies using these:
+#install dependencies by using these commands in terminal:
 #python3 -m pip install opencv-python
-#pip install tensorflow
+#pip install https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-1.8.0-py3-none-any.whl
 #pip install numpy
 
 
