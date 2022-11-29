1b0a2458a94f8454e59f7e55ca4d2b6f039470c9
NikhilMahalingam <mahalingamnikhil@gmail.com>
2022-11-29 14:46:13 -0500

    Install keras

--- index.py
+++ index.py
@@ -2,6 +2,7 @@
 #python3 -m pip install opencv-python
 #pip install https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-1.8.0-py3-none-any.whl
 #pip install numpy
+#pip install keras
 
 
 import tensorflow as tf
@@ -9,6 +10,7 @@ import os
 import uuid
 import time
 import cv2
+import keras
 
 labels = ['Nick Chen']
 number_imgs = 10
